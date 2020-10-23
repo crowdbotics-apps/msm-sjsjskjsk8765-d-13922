951543e8174587f59aadde43966542932453ed3b
Crowdbotics <team@crowdbotics.com>
2020-10-23 10:11:08 +0000

    Update components

--- src/features/BlankScreen013067/screens/blank.js
+++ src/features/BlankScreen013067/screens/blank.js
@@ -27,14 +27,9 @@ export default class Blank extends React.Component {
 
   render = () => (
     <View>
-      <View style={styles.View_2}>
-        <View />
-        <View />
-      </View>
-      <Text>Sample text content</Text>
-      <Text>Sample text content</Text>
+      <Text>Test Content</Text>
       <TextInput
-        placeholder="Sample text input placeholder"
+        placeholder="Test Placeholder"
         value={this.state.TextInput_7}
         onChangeText={nextValue => this.setState({ TextInput_7: nextValue })}
       />
@@ -47,11 +42,15 @@ const styles = StyleSheet.create({
     flex: 1,
     marginHorizontal: 16
   },
+
   View_1: {},
   View_2: { flexDirection: "row" },
   View_3: {},
   View_4: {},
   Text_5: {},
   Text_6: {},
+  TextInput_7: {},
+  View_1: {},
+  Text_5: {},
   TextInput_7: {}
 })
