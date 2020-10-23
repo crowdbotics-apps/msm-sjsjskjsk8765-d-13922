9124b1068b2980c992675c8036cfd440e8017b74
Crowdbotics <team@crowdbotics.com>
2020-10-23 10:09:34 +0000

    Update store

--- src/store/services.js
+++ src/store/services.js
@@ -1,2 +1,127 @@
 import axios from "axios"
-export const apiService = {}
+import {
+  DSSLACKCONNECTOR1_USERNAME,
+  DSSLACKCONNECTOR1_PASSWORD
+} from "react-native-dotenv"
+const dSSlackConnector = axios.create({
+  baseURL: "https://api.spotify.com",
+  auth: {
+    username: DSSLACKCONNECTOR1_USERNAME,
+    password: DSSLACKCONNECTOR1_PASSWORD
+  },
+  headers: { Accept: "application/json", "Content-Type": "application/json" }
+})
+const mSMsjsjskjskAPI = axios.create({
+  baseURL: "https://msm-sjsjskjsk8765-d-13922-prod.herokuapp.com/",
+  headers: { Accept: "application/json", "Content-Type": "application/json" }
+})
+function dsslackconnector1_get_v1_albums_list(action) {
+  return dSSlackConnector.get(`/v1/albums`, null, {
+    params: { ids: action.ids, market: action.market }
+  })
+}
+function api_v1_customtext_list(action) {
+  return mSMsjsjskjskAPI.get(`/api/v1/customtext/`)
+}
+function api_v1_customtext_read(action) {
+  return mSMsjsjskjskAPI.get(`/api/v1/customtext/${action.id}/`)
+}
+function api_v1_customtext_update(action) {
+  return mSMsjsjskjskAPI.put(`/api/v1/customtext/${action.id}/`, null, {
+    data: action.data
+  })
+}
+function api_v1_customtext_partial_update(action) {
+  return mSMsjsjskjskAPI.patch(`/api/v1/customtext/${action.id}/`, null, {
+    data: action.data
+  })
+}
+function api_v1_homepage_list(action) {
+  return mSMsjsjskjskAPI.get(`/api/v1/homepage/`)
+}
+function api_v1_homepage_read(action) {
+  return mSMsjsjskjskAPI.get(`/api/v1/homepage/${action.id}/`)
+}
+function api_v1_homepage_update(action) {
+  return mSMsjsjskjskAPI.put(`/api/v1/homepage/${action.id}/`, null, {
+    data: action.data
+  })
+}
+function api_v1_homepage_partial_update(action) {
+  return mSMsjsjskjskAPI.patch(`/api/v1/homepage/${action.id}/`, null, {
+    data: action.data
+  })
+}
+function api_v1_login_create(action) {
+  return mSMsjsjskjskAPI.post(`/api/v1/login/`)
+}
+function api_v1_signup_create(action) {
+  return mSMsjsjskjskAPI.post(`/api/v1/signup/`, null, { data: action.data })
+}
+function rest_auth_login_create(action) {
+  return mSMsjsjskjskAPI.post(`/rest-auth/login/`, null, { data: action.data })
+}
+function rest_auth_logout_list(action) {
+  return mSMsjsjskjskAPI.get(`/rest-auth/logout/`)
+}
+function rest_auth_logout_create(action) {
+  return mSMsjsjskjskAPI.post(`/rest-auth/logout/`)
+}
+function rest_auth_password_change_create(action) {
+  return mSMsjsjskjskAPI.post(`/rest-auth/password/change/`, null, {
+    data: action.data
+  })
+}
+function rest_auth_password_reset_create(action) {
+  return mSMsjsjskjskAPI.post(`/rest-auth/password/reset/`, null, {
+    data: action.data
+  })
+}
+function rest_auth_password_reset_confirm_create(action) {
+  return mSMsjsjskjskAPI.post(`/rest-auth/password/reset/confirm/`, null, {
+    data: action.data
+  })
+}
+function rest_auth_registration_create(action) {
+  return mSMsjsjskjskAPI.post(`/rest-auth/registration/`, null, {
+    data: action.data
+  })
+}
+function rest_auth_registration_verify_email_create(action) {
+  return mSMsjsjskjskAPI.post(`/rest-auth/registration/verify-email/`, null, {
+    data: action.data
+  })
+}
+function rest_auth_user_read(action) {
+  return mSMsjsjskjskAPI.get(`/rest-auth/user/`)
+}
+function rest_auth_user_update(action) {
+  return mSMsjsjskjskAPI.put(`/rest-auth/user/`, null, { data: action.data })
+}
+function rest_auth_user_partial_update(action) {
+  return mSMsjsjskjskAPI.patch(`/rest-auth/user/`, null, { data: action.data })
+}
+export const apiService = {
+  dsslackconnector1_get_v1_albums_list,
+  api_v1_customtext_list,
+  api_v1_customtext_read,
+  api_v1_customtext_update,
+  api_v1_customtext_partial_update,
+  api_v1_homepage_list,
+  api_v1_homepage_read,
+  api_v1_homepage_update,
+  api_v1_homepage_partial_update,
+  api_v1_login_create,
+  api_v1_signup_create,
+  rest_auth_login_create,
+  rest_auth_logout_list,
+  rest_auth_logout_create,
+  rest_auth_password_change_create,
+  rest_auth_password_reset_create,
+  rest_auth_password_reset_confirm_create,
+  rest_auth_registration_create,
+  rest_auth_registration_verify_email_create,
+  rest_auth_user_read,
+  rest_auth_user_update,
+  rest_auth_user_partial_update
+}
